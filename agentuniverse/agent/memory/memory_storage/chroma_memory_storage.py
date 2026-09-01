645f44f7a5683b696c31813a123eb203ad384510
Yue Wang <1939455790@qq.com>
2026-09-01 21:52:45 +0800

    docs: add docstring to is_nested_list in chroma_memory_storage.py

--- agentuniverse/agent/memory/memory_storage/chroma_memory_storage.py
+++ agentuniverse/agent/memory/memory_storage/chroma_memory_storage.py
@@ -227,4 +227,5 @@ class ChromaMemoryStorage(MemoryStorage):
 
     @staticmethod
     def is_nested_list(variable: List) -> bool:
+        """Check whether nested list."""
         return isinstance(variable, list) and len(variable) > 0 and isinstance(variable[0], list)
