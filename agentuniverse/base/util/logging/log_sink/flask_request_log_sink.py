136eee294f7e8655231e24de912c1e70a413a3b5
Yue Wang <1939455790@qq.com>
2026-09-04 22:44:59 +0800

    docs: add docstrings to flask_request_log_sink

--- agentuniverse/base/util/logging/log_sink/flask_request_log_sink.py
+++ agentuniverse/base/util/logging/log_sink/flask_request_log_sink.py
@@ -11,10 +11,17 @@ from agentuniverse.base.util.logging.log_type_enum import LogTypeEnum
 
 
 class FlaskRequestLogSink(BaseFileLogSink):
+    """File-based log sink dedicated to flask request records filtered by the flask_request log type.
+    """
     log_type: LogTypeEnum = LogTypeEnum.flask_request
 
 
     def process_record(self, record):
+        """Generate the sink message from the flask_request extra field and remove the field afterwards.
+
+        Args:
+            record: The loguru log record to process.
+        """
         record["message"] = self.generate_log(
             flask_request=record['extra']['flask_request']
         )
@@ -22,4 +29,9 @@ class FlaskRequestLogSink(BaseFileLogSink):
 
 
     def generate_log(self, flask_request) -> str:
+        """Placeholder generation hook that receives the flask request.
+
+        Args:
+            flask_request: The flask request object.
+        """
         pass
