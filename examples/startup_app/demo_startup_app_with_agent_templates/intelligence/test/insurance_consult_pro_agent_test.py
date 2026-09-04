002aa374992a8e53e2ffe97c20fa4dd11c4d8a0c
Yue Wang <1939455790@qq.com>
2026-09-05 00:15:06 +0800

    docs: add docstrings to insurance_consult_pro_agent_test

--- examples/startup_app/demo_startup_app_with_agent_templates/intelligence/test/insurance_consult_pro_agent_test.py
+++ examples/startup_app/demo_startup_app_with_agent_templates/intelligence/test/insurance_consult_pro_agent_test.py
@@ -12,6 +12,8 @@ AgentUniverse().start(config_path='../../config/config.toml', core_mode=True)
 
 
 def chat(question: str):
+    """Run a chat conversation with the agent and print the response.
+    """
     instance: Agent = AgentManager().get_instance_obj('insurance_consult_pro_agent')
     output = instance.run(input=question)
     return output.get_data('output')
